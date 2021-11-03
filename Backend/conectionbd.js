e60b798098d7b9ec21dc8920e90a0af159b0746e
edwinlg17 <2247574310101@ingenieria.usac.edu.gt>
2021-11-02 18:50:07 -0600

    agrego mi conexion

--- Backend/conectionbd.js
+++ Backend/conectionbd.js
@@ -1,13 +1,24 @@
-//mysql module
+// mysql module
 const mysql = require('mysql2');
+
+// //conexion mysql
+// module.exports = () =>{
+//     return mysql.createConnection({
+//         host :'192.168.8.134',
+//         user: 'root',
+//         password: '123456',
+//         database: 'ProyectoPracticas',
+//         port:3306
+//     }); 
+// }
 
 //conexion mysql
 module.exports = () =>{
     return mysql.createConnection({
-        host :'192.168.8.134',
+        host :'192.168.49.129',
         user: 'root',
         password: '123456',
-        database: 'ProyectoPracticas',
+        database: 'Practicas',
         port:3306
     }); 
 }
